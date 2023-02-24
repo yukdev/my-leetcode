cf429b823b918514309454e124458b9c700e1f80
Ken Yu <ken.yuwork@gmail.com>
2023-02-23 20:41:53 -0500

    fixed 1-two-sum docstring

--- 1-two-sum/twoSum.js
+++ 1-two-sum/twoSum.js
@@ -8,7 +8,7 @@
  * Example:
  * Input: nums = [3,2,4], target = 6
  * Output: [1,2]
- * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Explanation: Because nums[1] + nums[2] == 6, we return [1, 2].
  */
 
 /**
