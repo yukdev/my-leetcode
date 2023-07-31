6e7c9bb2c4d49e58e372bcbc18610ac9b03c860f
Ken Yu <ken.yuwork@gmail.com>
2023-07-31 14:48:14 -0400

    92 reverse linked list ii

---
+++ 92-reverse-linked-list-ii/reverseLinkedListII.js
@@ -0,0 +1,39 @@
+/**
+ * Given the head of a singly linked list
+ * and two integers left and right where left <= right,
+ * reverse the nodes of the list from position left to position right,
+ * and return the reversed list.
+ *
+ * Example 1:
+ * Input: head = [1,2,3,4,5], left = 2, right = 4
+ * Output: [1,4,3,2,5]
+ *
+ * Example 2:
+ * Input: head = [5], left = 1, right = 1
+ * Output: [5]
+ */
+const reverseBetween = (head, left, right) => {
+  if (!head || left === right) {
+    return head;
+  }
+
+  const dummyHead = new ListNode(null);
+  dummyHead.next = head;
+  let prev = dummyHead;
+
+  for (let i = 1; i < left; i++) {
+    prev = prev.next;
+  }
+
+  let current = prev.next;
+  let next = current.next;
+
+  for (let i = 0; i < right - left; i++) {
+    current.next = next.next;
+    next.next = prev.next;
+    prev.next = next;
+    next = current.next;
+  }
+
+  return dummyHead.next;
+};
