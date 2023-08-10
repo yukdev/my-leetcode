9b6193e0663ba21f034f8c5747d54793b1d4fa3f
Ken Yu <ken.yuwork@gmail.com>
2023-08-10 11:16:32 -0400

    unique paths w/ memoization

---
+++ 62-unique-paths/uniquePaths.js
@@ -0,0 +1,36 @@
+/**
+ * 62. Unique Paths
+ *
+ * There is a robot on an m x n grid.
+ * The robot is initially located at the top-left corner (i.e., grid[0][0]).
+ * The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+ * The robot can only move either down or right at any point in time.
+ *
+ * Given the two integers m and n,
+ * return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+ *
+ * The test cases are generated so that the answer will be less than or equal to 2 * 109.
+ *
+ * Example 1:
+ * Input: m = 3, n = 7
+ * Output: 28
+ *
+ * Example 2:
+ * Input: m = 3, n = 2
+ * Output: 3
+ * Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ * 1. Right -> Down -> Down
+ * 2. Down -> Down -> Right
+ * 3. Down -> Right -> Down
+ */
+const uniquePaths = (m, n, r = 0, c = 0, memo = {}) => {
+  const pos = r + ',' + c;
+  if (pos in memo) return memo[pos];
+
+  if (r === m || c === n) return 0;
+  if (r === m - 1 && c === n - 1) return 1;
+
+  memo[pos] =
+    uniquePaths(m, n, r + 1, c, memo) + uniquePaths(m, n, r, c + 1, memo);
+  return memo[pos];
+};
